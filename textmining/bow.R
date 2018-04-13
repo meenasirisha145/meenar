b648a8dc68df566e665bbfda533911b560e56b34
meena sirisha145 <meenasirisha145@gmail.com>
2018-04-13 22:09:48 +0530

    31/4/2018

--- textmining/bow.R
+++ textmining/bow.R
@@ -5,7 +5,7 @@ library(plotrix)
 library(dendextend)
 library(ggplot2)
 library(ggthemes)
-library(RWeka)
+
 
 data=read.csv("paytmtweet.csv",stringsAsFactors = FALSE)
 glimpse(data)
@@ -29,7 +29,7 @@ tweetcorpus <- tm_map(tweetcorpus, removeWords, stopwords("english"))
 tweetcorpus <- tm_map(tweetcorpus, stripWhitespace)
 tweetcorpus <- tm_map(tweetcorpus, removeNumbers)
 tweetcorpus[1]$content
-rmlist=c("@paytm","paytm","paytmMall","paytmBank","Paytm","xfxfxaxaxfxfxaxa")
+rmlist=c("@paytm","paytm","paytmMall","paytmBank","Paytm","xfxfxaxaxfxfxaxa","https","paytmcar","paytmbank")
 tweetcorpus <- tm_map(tweetcorpus, removeWords, rmlist)
 tweetcorpus[1]$content
 
@@ -62,5 +62,50 @@ barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
         col ="lightblue", main ="Most frequent words",
         ylab = "Word frequencies")
 
+dtm
+sparse <- removeSparseTerms(dtm, 0.99)
 
 
+score.sentiment = function(tweetcorpus, pos.words, neg.words)
+  
+{
+  
+  require(plyr)
+  require(stringr)
+  
+  scores = laply(tweetcorpus, function(tweet, pos.words, neg.words) {
+    word.list = str_split(tweet, '\\s+') # splits the tweets by word in a list
+    
+    words = unlist(word.list) # turns the list into vector
+    
+    pos.matches = match(words, pos.words) ## returns matching 
+    #values for words from list 
+    neg.matches = match(words, neg.words)
+    
+    pos.matches = !is.na(pos.matches) ## converts matching values to true of false
+    neg.matches = !is.na(neg.matches)
+    
+    score = sum(pos.matches) - sum(neg.matches) # true and false are 
+    #treated as 1 and 0 so they can be added
+    
+    return(score) 
+    
+  }, pos.words, neg.words )
+  
+  scores.df = data.frame(score=scores, text=tweets)
+  
+  return(scores.df)
+  
+}
+
+pos <- scan("positive-words.txt",what = 'character')
+neg<- scan("negative-words.txt",what = 'character')  
+analysis=score.sentiment(tweetcorpus,pos,neg)  
+analysis
+length(analysis)
+dim(analysis)
+colnames(analysis)
+analysis$score
+analysis$sentiment=ifelse(analysis$score>0,"positive",ifelse(analysis$score<0,"negative","neutral"))
+head(analysis)
+write.csv(analysis,"sentiment.csv",row.names = FALSE)
--- textmining/naivebayes.R
+++ textmining/naivebayes.R
@@ -9,5 +9,57 @@ library(caret)
 # Library for parallel processing
 library(doMC)
 registerDoMC(cores=detectCores())  # Use all available cores
-data=read.csv("paytmtweet.csv",stringsAsFactors = FALSE)
+data=read.csv("sentiment.csv",stringsAsFactors = FALSE)
 glimpse(data)
+data$sentiment=as.factor(data$sentiment)
+
+corpus <- Corpus(VectorSource(data$text))
+# Inspect the corpus
+corpus
+
+rmlist=c("@paytm","paytm","paytmMall","paytmBank","Paytm","xfxfxaxaxfxfxaxa","https","paytmcar","paytmbank")
+# Use dplyr's  %>% (pipe) utility to do this neatly.
+corpus.clean <- corpus %>%
+  tm_map(content_transformer(tolower)) %>% 
+  tm_map(removePunctuation) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removeWords, stopwords(kind="en")) %>%
+  tm_map(stripWhitespace) %>%
+  tm_map(stemDocument) %>%
+  tm_map(removeWords,rmlist)
+corpus.clean
+dtm <- DocumentTermMatrix(corpus.clean)
+# Inspect the dtm
+inspect(dtm[40:50, 10:15])
+
+
+df.train <- data[1:300,]
+df.test <- data[301:500,]
+
+dtm.train <- dtm[1:300,]
+dtm.test <- dtm[301:500,]
+
+corpus.clean.train <- corpus.clean[1:300]
+corpus.clean.test <- corpus.clean[301:500]
+
+dim(dtm.train)
+
+onefreq <- findFreqTerms(dtm.train, 1)
+length((onefreq))
+
+dtm.train.nb <- DocumentTermMatrix(corpus.clean.train, control=list(dictionary = onefreq))
+
+dim(dtm.train.nb)
+
+dtm.test.nb <- DocumentTermMatrix(corpus.clean.test, control=list(dictionary = onefreq))
+
+dim(dtm.test.nb)
+dtm.train.nbmat=as.matrix(dtm.train.nb)
+
+
+classifier <- naiveBayes(dtm.train.nbmat,as.factor(data[1:300,3]))
+pred=predict(classifier,newdata=df.test)
+table(df.test[,3],pred)
+inspect(dtm.train.nb)
+length(trainNB)
+length(df.train$class)
