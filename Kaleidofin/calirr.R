ab049d467d79496a555dd6ba75b7f8505fb8f6f3
meena sirisha145 <meenasirisha145@gmail.com>
2018-04-13 21:10:20 +0530

    13/04/2018

--- Kaleidofin/calirr.R
+++ Kaleidofin/calirr.R
@@ -1,24 +1,21 @@
 #Loading the Data
 data=read.csv("irr.csv",stringsAsFactors = FALSE)
 head(data)
-
-for (i in nrow(data)){
-  irr=data$Interest.Premium/data$Totalamount.to.pay
-  data$irr=irr
+months=40
+library(FinCal)
+#There is a function called IRR in Fincal package to calculate internal rate of return
+for (i in 1:nrow(data)){
+  
+  cf0 =-1*(data$Yield[i])
+  cf1=c(rep(data$Installment[i],(i-1)),rep(data$After.Lifting[i],(months-(i-1))))
+  cf2=c(cf0,cf1)
+  irrcal=irr(cf2)
+  data$cal[i]=irrcal
 }
 head(data)
+data
+write.csv(data,"solirr.csv",row.names=FALSE)
 
-library(FinCal)
-cf1=c(-20300,rep(5000,3),rep(7000,37))
-
-irr(cf1)
-IRR=function(Duration,inst_lft,inst_nonlft,lft_time)
-{
-  yield=Duration*inst_lft+(lft_time-1)*1000
-  totalamt=(lft_time-1)*inst_lft+(Duration-lft_time+1)*inst_nonlft
-  irr=(-yield+totalamt)/totalamt
-  return(irr)
-}
 
 
 
